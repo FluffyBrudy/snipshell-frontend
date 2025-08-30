ffb70b2e7298376704e59f3d4aec806e82aeef2d
rudy <fluffyrudy@proton.me>
2025-08-30 21:08:32 +0545

    add description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,27 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "snip-shell",
-  description: "CLI snippet management web application for linux users",
+  title: "SnipShell - Your Personal CLI Snippet Manager",
+  description:
+    "SnipShell is a simple and effective web app for storing, managing, and organizing your Linux CLI commands, aliases, and notes.",
+  keywords:
+    "Linux, command, snippets, alias, terminal, CLI, Linux commands, snippet manager",
+  authors: [{ name: "FluffyRudy" }, { name: "FluffyBrudy" }],
+  openGraph: {
+    type: "website",
+    url: "https://github.com/FluffyBrudy/SnipShell",
+    title: "SnipShell - Your Personal CLI Snippet Manager",
+    description:
+      "Store and organize your Linux CLI commands and aliases in one place. Simplify your command-line experience.",
+    images: [
+      {
+        url: "https://github.com/FluffyBrudy/SnipShell/logo.png",
+        width: 800,
+        height: 600,
+        alt: "SnipShell Logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
